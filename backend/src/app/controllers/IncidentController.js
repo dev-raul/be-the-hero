e39aecf1bc6bf208bb24db23200230c244138fae
Raul Silva Sousa <rraulsilva.dev@gmail.com>
2020-03-25 00:50:52 -0300

    Added paginate to a incidents!

--- backend/src/app/controllers/IncidentController.js
+++ backend/src/app/controllers/IncidentController.js
@@ -2,7 +2,22 @@ import Db from "../../database";
 
 class IncidentController {
   async index(req, res) {
-    const incidents = await Db("incidents").select("*");
+    const { page = 1 } = req.query;
+    const [count] = await Db("incidents").count();
+    const incidents = await Db("incidents")
+      .join("ongs", "ongs.id", "=", "incidents.ong_id")
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select([
+        "incidents.*",
+        "ongs.name",
+        "ongs.email",
+        "ongs.whatsapp",
+        "ongs.city",
+        "ongs.uf"
+      ]);
+
+    res.header("X-Total-Counter", count["count(*)"]);
     return res.json(incidents);
   }
   async store(req, res) {
